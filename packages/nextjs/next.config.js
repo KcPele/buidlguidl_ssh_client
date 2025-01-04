4211a2138fc49e7387eef03c05145407d9f60db2
KcPele <fidekg123@gmail.com>
2025-01-04 10:58:08 +0100

    removed fleek folder

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -9,9 +9,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: config => {
+  webpack: (config, { isServer }) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
+    if (isServer) {
+      config.externals.push("ssh2"); // Exclude `ssh2` from the server bundle
+    }
 
     return config;
   },
